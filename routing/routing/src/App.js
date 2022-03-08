0f67f7019b7fd4bc538ccd51466a44e7479070f6
lies93 <hocini.lies@gmail.com>
2022-03-08 15:39:17 +0100

    Add Name , price and img done

--- routing/routing/src/App.js
+++ routing/routing/src/App.js
@@ -5,19 +5,23 @@ import React from 'react';
 
 import Home from './component/home';
 import Produits from './component/Produits';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 
 
 class App extends React.Component{
   render(){
     return(
       <div className='App'>
-        {/* <Produits></Produits>
-        <Home></Home> */}
+        <header>
+            <nav>
+              <Link to="/">Home</Link>
+              <Link to="/products">Produits</Link>
+            </nav>
+
+        </header>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/products" element={<Produits/>}/>
-
         </Routes>
       </div>
     )
--- routing/routing/src/component/Produits.jsx
+++ routing/routing/src/component/Produits.jsx
@@ -1,45 +1,56 @@
 import React from "react";
-import axios from "axios" ;
+import axios from "axios";
 
 
 
 
-class Produits extends React.Component{
-   constructor(){
-       super();
-       this.state={
-           products:[],
+class Produits extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            products: [],
         }
 
     }
-    componentDidMount = () =>{
-        axios.get("https://fakestoreapi.com/products/").then((result)=>{
+    componentDidMount = () => {
+        axios.get("https://fakestoreapi.com/products/").then((result) => {
             console.log("result", result);
             console.log('data', result.data);
             this.setState({
-                products:result.data,
+                products: result.data,
             })
-    
-}
-)
-    
-    
-        
-   }
 
-
-   
-   
-    render(){
-        return(
-            <div>
-               {this.state.products.map((product)=>
-                <p>
-                    {product.title}
-                </p>)}
-            </div>
+        }
         )
+
+
+
     }
+
+
+
+
+    render() {
+        return(
+            <>
+                {this.state.products.map(product => {
+
+                    return (
+
+                        <div>
+                            <p>{product.title}</p>
+                            <p>{product.price}</p>
+                            <img src={product.image} height="150px"/>
+                        </div>
+                    )
+                }
+                
+                
+                
+                )      
+                
+            }
+            </>)}
 }
 
 export default Produits;
