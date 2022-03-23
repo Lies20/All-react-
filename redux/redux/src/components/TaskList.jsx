99d0eaad0b91ac19cb32eb9c91fc97459aafb2ec
lies93 <hocini.lies@gmail.com>
2022-03-23 14:21:02 +0100

    step 3 done

--- redux/redux/src/components/TaskList.jsx
+++ redux/redux/src/components/TaskList.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React,{useContext} from "react";
+import { TacheContext } from "../App";
 
- export default function TaskList (){
+
+
+export default function TaskList (){
+    
+    const tacheContext = useContext(TacheContext)
+    console.log("test",tacheContext);
+
+    
     return(
         <>
         <h1>Liste de tâches </h1>
+        {tacheContext.taches.map((tache)=>{
+            // console.log(tache)
+            return(
+                <div>
+                    <h2>{tache.title}</h2>
+                    <p>{tache.priority}</p>
+                </div>
+            )
+        })}
         </>
         
     )
