29ac457277025e37ad61d3f3cc563fefc9e3f9bc
lies93 <hocini.lies@gmail.com>
2022-03-23 13:44:19 +0100

    step 2 done

--- redux/redux/src/App.js
+++ redux/redux/src/App.js
@@ -2,14 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import TaskList from './components/TaskList';
 import Task from './components/Task';
+import { useState } from 'react';
+import React from 'react';
+
+export const TacheContext =React.createContext();
 
 function App() {
+  const [taches,setTaches]=useState([
+
+    { id: 1, title: 'Tache 01', priority: 'Important'},        
+    { id: 2, title: 'Tache 02', priority: 'Urgent'}   
+  ])
+    
   return (
+    <TacheContext.Provider value={{taches,setTaches}}>
     <h1>
         hello world
-        <p><TaskList/></p>
-        <p><Task/></p>
+        <TaskList/>
+        <Task/>
     </h1>
+    </TacheContext.Provider>
   );
 }
 
